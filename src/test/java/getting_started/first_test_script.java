73448829a7e7f8b2106932ec932649c892df84a6
Rohit Rawat <rohit186201@gmail.com>
2026-04-20 15:52:54 +0530

    initial commit

---
+++ src/test/java/getting_started/first_test_script.java
@@ -0,0 +1,23 @@
+package getting_started;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+public class first_test_script {
+    public static void main(String[] args) {
+        Playwright pw = Playwright.create();
+        BrowserType browsertype = pw.chromium();
+        Browser browser = browsertype.launch(new BrowserType.LaunchOptions().setHeadless(false));
+        Page page = browser.newPage();
+        page.navigate("https://www.google.com");
+        String title = page.title();
+        System.out.println("Title = " + title);
+
+        //Closing the test
+        page.close();
+        browser.close();
+        pw.close();
+    }
+}
